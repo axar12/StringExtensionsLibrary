6b995d716ab0621c36dc4548c01a473145c80617
Akshar Patel <apate900@my.centennialcollege.ca>
2024-12-01 20:35:20 -0500

    Initial commit with .gitignore

---
+++ StringExtensionsLibrary/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringExtensionsLibrary
+{
+    public static class StringHelper
+    {
+        /// <summary>
+        /// Checks if the string starts with an uppercase letter.
+        /// </summary>
+        /// <param name="str">The input string to check.</param>
+        /// <returns>True if the string starts with an uppercase letter; otherwise, false.</returns>
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char firstChar = str[0];
+            return char.IsUpper(firstChar);
+        }
+    }
+}
